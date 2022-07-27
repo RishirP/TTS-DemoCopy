d3c1b47e663256f3cf81ca28e1c88c30a02af6b7
Matthew Cross <matthewcross1017@gmail.com>
2022-07-27 11:56:25 -0400

    add polly

---
+++ TTS.js
@@ -0,0 +1,6 @@
+// Imports the Google Cloud client library
+var polly = new AWS.Polly();
+polly.deleteLexicon(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
