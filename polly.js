fe7f63ae9dc508602715af4471dcefd84f4a32c1
Matthew Cross <matthewcross1017@gmail.com>
2022-08-16 21:19:32 -0400

    get boundary .text

--- polly.js
+++ polly.js
@@ -20,8 +20,6 @@ import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 import { Polly, StartSpeechSynthesisTaskCommand } from "@aws-sdk/client-polly";
 import { getSynthesizeSpeechUrl } from "@aws-sdk/polly-request-presigner";
-
-// Create the Polly service client, assigning your credentials
 const client = new Polly({
   region: "us-east-1",
   credentials: fromCognitoIdentityPool({
@@ -32,13 +30,13 @@ const client = new Polly({
 
 // Set the parameters
 const speechParams = {
-  OutputFormat: "mp3", // For example, 'mp3'
+  OutputFormat: "json", // For example, 'mp3'
   SampleRate: "16000", // For example, '16000
   OutputS3BucketName: "testbucket",
   Text: "", // The 'speakText' function supplies this value
   TextType: "text", // For example, "text"
   VoiceId: "Matthew", // For example, "Matthew",
-  // 'SpeechMarkTypes' : ["word"]
+  'SpeechMarkTypes' : ["word"]
 };
 // snippet-end:[Polly.JavaScript.BrowserExample.configV3]
 // snippet-start:[Polly.JavaScript.BrowserExample.synthesizeV3]
@@ -51,9 +49,15 @@ const speakText = async () => {
       params: speechParams,
     });
     console.log(url);
+    // Get the speech marks
+        fetch(url)
+    .then((response) => response.text())
+    .then((data) => console.log(data));
+
     // fetch(url)
-    // .then((response) => response.text())
-    // .then((data) => console.log(data));
+    // .then(response => response.text()).then(data => 
+    // console.log(data));
+
     const run = async () => {
       try {
         const data = await client.send(
@@ -64,17 +68,23 @@ const speakText = async () => {
           "Success, audio file added to " + speechParams.OutputS3BucketName
         );
         let requestId = data.SynthesisTask.TaskId;
-        console.log('The request ID is' + requestId)
-        const retrieveAudioParams =  {
-            TaskId: ' ' /* required */
-          };
-        retrieveAudioParams.TaskId = requestId.toString()
-        console.log('This is the retrival params' + retrieveAudioParams.TaskId)
-        client.getSpeechSynthesisTask(retrieveAudioParams, function(err, data) {
-            if (err) console.log(err, err.stack, AWS.Response); // an error occurred
-            else     console.log(data);           // successful response
-          });
-    } catch (err) {
+        console.log("The request ID is " + requestId);
+        const retrieveAudioParams = {
+          TaskId: " " /* required */,
+        };
+        retrieveAudioParams.TaskId = requestId.toString();
+        console.log(
+          "This is the retrival params " + retrieveAudioParams.TaskId
+        );
+        client.getSpeechSynthesisTask(
+          retrieveAudioParams,
+          function (err, data) {
+            if (err)
+              console.log(err, err.stack, AWS.Response); // an error occurred
+            else console.log(data); // successful response
+          }
+        );
+      } catch (err) {
         console.log("Error putting object", err);
       }
     };
