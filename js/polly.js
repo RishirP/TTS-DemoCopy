d80978ad124b9a86cd8b1853bdfb75c2d5e816e8
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-09-02 13:15:37 -0400

    Update polly.js

--- js/polly.js
+++ js/polly.js
@@ -142,7 +142,7 @@ let prev_time = 0;
 let i = 0;
 //  where text is highlighted   
 const timingfunc = function () {
-  console.log(readBlockElement)
+  console.log(readBlock)
 console.log("time to highlight");
 console.log(content[0]);
 console.log(highlightArray)
